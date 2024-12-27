4e90ff56d697d82ecd14b3f97a819477f059bf12
satish <satishnda3576@gmail.com>
2024-12-27 20:14:50 +0530

    hero section is ready

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero  from './components/hero'
+function App() {
+  return (
+    <div className='relative h-dvh w-screen overflow-x-hidden' >
+      
+      <h1 className='text-3xl font-bold underline text-red-500 font-circular-web'></h1>
+      
+      
+      <Hero/>
+  
+
+    </div>
+  )
+}
+
+export default App
