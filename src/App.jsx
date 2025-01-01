3bc49a3ef9c36d438f95d5eecfe2ff873d074d64
satish <satishnda3576@gmail.com>
2025-01-01 13:11:59 +0530

    Completed the whole project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import Hero  from './components/hero'
+
+import NavBar from './components/Navbar';
+import Features from './components/Features';
+import FloatingImage from './components/story';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden' >
       
       <h1 className='text-3xl font-bold underline text-red-500 font-circular-web'></h1>
       
-      
+      <NavBar/>
       <Hero/>
-  
+      
+      <Features/>
+      <FloatingImage/>
+      <Contact/>
+      <Footer/>
 
     </div>
   )
